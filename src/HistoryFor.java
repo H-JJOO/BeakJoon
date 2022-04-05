f9e5f41e498578bb7ea3e4fe9419f38ef7ab7f8f
koreait-shopping <icepri3535@gmail.com>
2022-04-05 16:42:20 +0900

    Bronze3 [FOR 진행중, TODO A+B - 5]

--- src/HistoryFor.java
+++ src/HistoryFor.java
@@ -162,4 +162,28 @@ public class HistoryFor {
             System.out.println();
         }
     }
+
+    //정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+    public static void smallNum(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < a; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if (value < b) {
+                sb.append(value).append(' ');
+            }
+        }
+        System.out.println(sb);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 1; i <= a; i++) {
-            for (int j = 1; j <= a - i; j++) {
-                System.out.print(" ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < a; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if (value < b) {
+                sb.append(value).append(' ');
             }
-            for (int k = 1; k <= i; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
         }
+        System.out.println(sb);
+
     }
 }
