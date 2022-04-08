67aa74ab45eef342695001bf69c8456ba686f415
koreait-shopping <icepri3535@gmail.com>
2022-04-08 22:07:59 +0900

    Bronze2 [1차원 배열, TODO 최댓값]

--- src/HistoryList.java
+++ src/HistoryList.java
@@ -1,10 +1,17 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class HistoryList {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+
+    }
+
+    //N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+    public static void maxMin(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int a = Integer.parseInt(br.readLine());
