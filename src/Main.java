55ce35fa9217d29dd72862ac0f56950872f19155
koreait-shopping <icepri3535@gmail.com>
2022-04-09 19:00:13 +0900

    Bronze2 [1차원 배열, TODO 숫자의 개수]

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int a = Integer.parseInt(br.readLine());
+        int[] arr = new int[9];
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+        for (int i = 0; i < 9; i ++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        int max = 0;
         int index = 0;
 
-        int[] arr = new int[a];
+        int count = 0;
 
-        while (st.hasMoreTokens()) {
-            arr[index] = Integer.parseInt(st.nextToken());
-            index++;
+        for (int value : arr) {
+            count++;
+            if (value > max) {
+                max = value;
+                index = count;
+            }
         }
-        Arrays.sort(arr);//배열 오름차순 정렬
-        System.out.println(arr[0] + " " + arr[a - 1]);
+        System.out.println(max);
+        System.out.println(index);
     }
 }
