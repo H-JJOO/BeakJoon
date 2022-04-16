05c5a27acdc7092403591a6e08ad09296ae1a892
koreait-shopping <icepri3535@gmail.com>
2022-04-16 19:31:40 +0900

    Bronze1 [함수 진행중, TODO 셀프 넘버(review), 한수]

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,34 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int [] i = {1,2,3};
-        System.out.println(new Test().sum(i));
+
+        boolean[] check = new boolean[10001];
+
+        for (int i = 0; i < 10001; i++) {
+            int n = d(i);
+
+            if (n < 10001) {
+                check[n] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < 10001; i++) {
+            if (!check[i]) {
+                sb.append(i).append('\n');
+            }
+        }
+        System.out.println(sb);
     }
 
+    public static int d(int number) {
+        int sum = number;
+
+        while (number != 0) {
+            sum = sum + (number % 10);//첫 자리
+            number = number / 10;//첫 째 자리 삭제
+        }
+        return sum;
+    }
 }
