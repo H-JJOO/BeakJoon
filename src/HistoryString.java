d10b56a4074842225a01edfec2b6de5006428c49
koreait-shopping <icepri3535@gmail.com>
2022-04-21 22:38:04 +0900

    Bronze1 [문자열 진행 중, TODO 셀프 넘버(review), 한수(review), 문자열 반복(review), 단어 공부]

--- src/HistoryString.java
+++ src/HistoryString.java
@@ -61,4 +61,28 @@ public class HistoryString {
         }
     }
 
+    //문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
+    public static void repeat(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++) {
+
+            String[] str = br.readLine().split(" ");	// 공백 분리
+
+            int R = Integer.parseInt(str[0]);	// String -> int
+
+            for (byte val : str[1].getBytes()) {
+                for (int j = 0; j < R; j++) {
+                    sb.append((char)val);
+                }
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,25 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int [] arr = new int[26];//소문자 a~z
+        int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = -1;//초기화
-        }
+        for(int i = 0; i < T; i++) {
 
-        String S = br.readLine();//입력 단어
+            String[] str = br.readLine().split(" ");	// 공백 분리
 
-        for (int i = 0; i < S.length(); i++) {
-            char ch = S.charAt(i);
+            int R = Integer.parseInt(str[0]);	// String -> int
 
-            if (arr[ch - 'a'] == -1) {//arr 값이 -1 인 경우 초기화
-                arr[ch - 'a'] = i;
+            for (byte val : str[1].getBytes()) {
+                for (int j = 0; j < R; j++) {
+                    sb.append((char)val);
+                }
             }
+            sb.append('\n');
         }
-
-        for (int val : arr) {//배열 출력
-            System.out.print(val + " ");
-        }
+        System.out.println(sb);
     }
 }
