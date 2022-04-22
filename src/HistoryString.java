b5abd2d6d81a0771c77918d553157af6ff1de9b0
koreait-shopping <icepri3535@gmail.com>
2022-04-22 22:10:25 +0900

    Silver5 [문자열 진행 중, TODO 셀프 넘버(review), 한수(review), 문자열 반복(review), 단어 공부(review),단어의 개수]

--- src/HistoryString.java
+++ src/HistoryString.java
@@ -84,5 +84,34 @@ public class HistoryString {
         }
         System.out.println(sb);
     }
+    //알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+    public static void manySpelling(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];//영문자 개수
+        String s = br.readLine();// 입력단어
+
+        for (int i = 0; i < s.length(); i++) {
+            if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {//대문자 범위
+                arr[s.charAt(i) - 'a']++;//인덱스 값 1 증가
+            } else {//소문자 범위
+                arr[s.charAt(i) - 'A']++;
+            }
+        }
+        int max = -1;
+        char ch = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65);//대문자 출력
+            }
+            else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.println(ch);
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
+        int[] arr = new int[26];//영문자 개수
+        String s = br.readLine();// 입력단어
 
-        for(int i = 0; i < T; i++) {
-
-            String[] str = br.readLine().split(" ");	// 공백 분리
-
-            int R = Integer.parseInt(str[0]);	// String -> int
-
-            for (byte val : str[1].getBytes()) {
-                for (int j = 0; j < R; j++) {
-                    sb.append((char)val);
-                }
+        for (int i = 0; i < s.length(); i++) {
+            if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {//대문자 범위
+                arr[s.charAt(i) - 'a']++;//인덱스 값 1 증가
+            } else {//소문자 범위
+                arr[s.charAt(i) - 'A']++;
             }
-            sb.append('\n');
         }
-        System.out.println(sb);
+        int max = -1;
+        char ch = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65);//대문자 출력
+            }
+            else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.println(ch);
     }
 }
