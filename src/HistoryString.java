adc82284a71178425a47d0cbbadc26bdc61d3fcf
koreait-shopping <icepri3535@gmail.com>
2022-04-25 15:59:36 +0900

    Silver5 [문자열 진행 중, TODO 셀프 넘버(review), 한수(review), 문자열 반복(review), 단어 공부(review),단어의 개수(review), 크로아티아 알파벳]

--- src/HistoryString.java
+++ src/HistoryString.java
@@ -173,4 +173,48 @@ public class HistoryString {
         System.out.println(a > b ? a : b);
     }
 
+    //전화를 걸고 싶은 번호가 있다면, 숫자를 하나를 누른 다음에 금속 핀이 있는 곳 까지 시계방향으로 돌려야 한다. 숫자를 하나 누르면 다이얼이 처음 위치로 돌아가고, 다음 숫자를 누르려면 다이얼을 처음 위치에서 다시 돌려야 한다.
+    //숫자 1을 걸려면 총 2초가 필요하다. 1보다 큰 수를 거는데 걸리는 시간은 이보다 더 걸리며, 한 칸 옆에 있는 숫자를 걸기 위해선 1초씩 더 걸린다.
+    //상근이의 할머니는 전화 번호를 각 숫자에 해당하는 문자로 외운다. 즉, 어떤 단어를 걸 때, 각 알파벳에 해당하는 숫자를 걸면 된다. 예를 들어, UNUCIC는 868242와 같다.
+    //할머니가 외운 단어가 주어졌을 때, 이 전화를 걸기 위해서 필요한 최소 시간을 구하는 프로그램을 작성하시오.
+    public static void phone(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = String.format(br.readLine());
+
+        int sec = 0;
+
+        for (int i = 0; i < a.length(); i++) {
+
+            switch (a.charAt(i)) {
+                case 'A' : case 'B' : case 'C' :
+                    sec += 3;
+                    break;
+                case 'D' : case 'E' : case 'F' :
+                    sec += 4;
+                    break;
+                case 'G' : case 'H' : case 'I' :
+                    sec += 5;
+                    break;
+                case 'J' : case 'K' : case 'L' :
+                    sec += 6;
+                    break;
+                case 'M' : case 'N' : case 'O' :
+                    sec += 7;
+                    break;
+                case 'P' : case 'Q' : case 'R' : case 'S' :
+                    sec += 8;
+                    break;
+                case 'T' : case 'U' : case 'V' :
+                    sec += 9;
+                    break;
+                case 'W' : case 'X' : case 'Y' : case 'Z' :
+                    sec += 10;
+                    break;
+            }
+        }
+        System.out.println(sec);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -7,12 +6,40 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String a = String.format(br.readLine());
 
-        //StringBuilder 의 reverse 메소드
-        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int sec = 0;
 
-        System.out.println(a > b ? a : b);
+        for (int i = 0; i < a.length(); i++) {
+
+            switch (a.charAt(i)) {
+                case 'A' : case 'B' : case 'C' :
+                    sec += 3;
+                    break;
+                case 'D' : case 'E' : case 'F' :
+                    sec += 4;
+                    break;
+                case 'G' : case 'H' : case 'I' :
+                    sec += 5;
+                    break;
+                case 'J' : case 'K' : case 'L' :
+                    sec += 6;
+                    break;
+                case 'M' : case 'N' : case 'O' :
+                    sec += 7;
+                    break;
+                case 'P' : case 'Q' : case 'R' : case 'S' :
+                    sec += 8;
+                    break;
+                case 'T' : case 'U' : case 'V' :
+                    sec += 9;
+                    break;
+                case 'W' : case 'X' : case 'Y' : case 'Z' :
+                    sec += 10;
+                    break;
+            }
+        }
+        System.out.println(sec);
     }
+
 }
