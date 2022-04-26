7070882dcbbeeaa6a4f99a6ab5fc4777f65dcd9b
koreait-shopping <icepri3535@gmail.com>
2022-04-26 16:26:06 +0900

    Silver5 [문자열 진행 중, TODO 셀프 넘버(review), 한수(review), 문자열 반복(review), 단어 공부(review),단어의 개수(review), 그룹 단어 체커]

--- src/HistoryString.java
+++ src/HistoryString.java
@@ -216,5 +216,46 @@ public class HistoryString {
         }
         System.out.println(sec);
     }
+    //크로아티아 알파벳 개수가 몇개 사용되었는지 출력하는 프로그램
+
+    public static void cAC(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        int len = str.length();
+
+        int cnt = 0;//크로아티아 알파벳 개수
+
+        for (int i = 0; i < len; i++) {
+
+            char ch = str.charAt(i);//입력한 단어의 i index 문자
+
+            if (ch == 'c' && i < len - 1) {//만약 ch == c 라면
+                if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {//만약 ch 다음 문자가 '=' 또는 '-'라면
+                    i++;//i+1 까지가 하나의 문자이므로 다음 문자 건너뛰기
+                }
+            } else if (ch == 'd' && i < len - 1) {
+                if (str.charAt(i + 1) == '-') {//d-
+                    i++;
+                } else if (str.charAt(i + 1) == 'z' && i < len - 2) {
+                    if (str.charAt(i + 2) == '=') {//dz=
+                        i += 2;
+                    }
+                }
+            } else if ((ch == 'l' || ch == 'n') && i < len - 1) {
+                if (str.charAt(i + 1) == 'j') {//lj || nj
+                    i++;
+                }
+            } else if ((ch == 's' || ch == 'z') && i < len - 1) {
+                if (str.charAt(i + 1) == '=') {//s= || z=
+                    i++;
+                }
+            }
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,40 +6,39 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String a = String.format(br.readLine());
+        String str = br.readLine();
 
-        int sec = 0;
+        int len = str.length();
 
-        for (int i = 0; i < a.length(); i++) {
+        int cnt = 0;//크로아티아 알파벳 개수
 
-            switch (a.charAt(i)) {
-                case 'A' : case 'B' : case 'C' :
-                    sec += 3;
-                    break;
-                case 'D' : case 'E' : case 'F' :
-                    sec += 4;
-                    break;
-                case 'G' : case 'H' : case 'I' :
-                    sec += 5;
-                    break;
-                case 'J' : case 'K' : case 'L' :
-                    sec += 6;
-                    break;
-                case 'M' : case 'N' : case 'O' :
-                    sec += 7;
-                    break;
-                case 'P' : case 'Q' : case 'R' : case 'S' :
-                    sec += 8;
-                    break;
-                case 'T' : case 'U' : case 'V' :
-                    sec += 9;
-                    break;
-                case 'W' : case 'X' : case 'Y' : case 'Z' :
-                    sec += 10;
-                    break;
+        for (int i = 0; i < len; i++) {
+
+            char ch = str.charAt(i);//입력한 단어의 i index 문자
+
+            if (ch == 'c' && i < len - 1) {//만약 ch == c 라면
+                if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {//만약 ch 다음 문자가 '=' 또는 '-'라면
+                    i++;//i+1 까지가 하나의 문자이므로 다음 문자 건너뛰기
+                }
+            } else if (ch == 'd' && i < len - 1) {
+                if (str.charAt(i + 1) == '-') {//d-
+                    i++;
+                } else if (str.charAt(i + 1) == 'z' && i < len - 2) {
+                    if (str.charAt(i + 2) == '=') {//dz=
+                        i += 2;
+                    }
+                }
+            } else if ((ch == 'l' || ch == 'n') && i < len - 1) {
+                if (str.charAt(i + 1) == 'j') {//lj || nj
+                    i++;
+                }
+            } else if ((ch == 's' || ch == 'z') && i < len - 1) {
+                if (str.charAt(i + 1) == '=') {//s= || z=
+                    i++;
+                }
             }
+            cnt++;
         }
-        System.out.println(sec);
+        System.out.println(cnt);
     }
-
 }
