4f785ce19ba64a1edfe5b177fc2b7e9ed8bc60d2
koreait-shopping <icepri3535@gmail.com>
2022-04-28 17:18:02 +0900

    Silver5 [기본 수학 1 진행중, TODO 셀프 넘버(review), 한수(review), 문자열 반복(review), 단어 공부(review),단어의 개수(review), 그룹 단어 체커(review), 벌집]

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,26 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws IOException {
 
-        int cnt = 0;
-        int N = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < N; i++) {
-            if (check() == true) {
-                cnt++;
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        //A 고정비용(단발), B 가면 비용(재료비, 인건비), C 노트북 가격
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if (C <= B) {
+            System.out.println("-1");
+        } else {
+            System.out.println(A / (C - B) + 1);
         }
-        System.out.println(cnt);
     }
 
-    //중복 체크 함수
-    public static boolean check() throws IOException {
-        boolean[] check = new boolean[26];//알파벳
-        int prev = 0;
-        String str = br.readLine();
 
-        for (int i = 0; i < str.length(); i++) {
-            int now = str.charAt(i);//현재 문자
-
-            if (prev != now) {//전문자와 i q번째 문자가 다르다면
-                if (check[now - 'a'] == false) {//처음나오는 문자라면
-                    check[now - 'a'] = true;//true 로 변환
-                    prev = now;//초기화
-                } else {
-                    return false;//함수종료
-                }
-            }
-            else {
-                continue;
-            }
-        }
-        return true;
-    }
 }
