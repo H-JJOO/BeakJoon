60f0c6ef3ea6478d4c0756c7c6d4838c05d46912
koreait-shopping <icepri3535@gmail.com>
2022-05-01 19:35:48 +0900

    Silver5 [기본 수학 1 진행중(+새싹), TODO 셀프 넘버(review), 한수(review), 문자열 반복(review), 단어 공부(review),단어의 개수(review), 그룹 단어 체커(review), 분수찾기(review), 달팽이는 올라가고 싶다]

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,31 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("         ,r'\"7");
-        System.out.println("r`-_   ,'  ,/");
-        System.out.println(" \\. \". L_r'");
-        System.out.println("   `~\\/");
-        System.out.println("      |");
-        System.out.println("      |");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+
+        int cross_count = 1;
+        int prev_count_sum = 0;
+
+        while (true) {
+            // 직전 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
+            if (a <= prev_count_sum + cross_count) {
+                if (cross_count % 2 == 1) {	// 대각선의 개수가 홀수라면
+                    // 분모가 큰 수부터 시작
+                    // 분모는 대각선 개수 - (a 번째 - 직전 대각선까지의 누적합 - 1)
+                    // 분자는 a 번째 - 직전 대각선까지의 누적합
+                    System.out.print((cross_count - (a - prev_count_sum - 1)) + "/" + (a - prev_count_sum));
+                    break;
+                }
+                else {	// 대각선의 개수가 짝수라면
+                    // 홀수일 때의 출력을 반대로
+                    System.out.print((a - prev_count_sum) + "/" + (cross_count - (a - prev_count_sum - 1)));
+                    break;
+                }
+            } else {
+                prev_count_sum += cross_count;
+                cross_count++;
+            }
+        }
     }
 }
