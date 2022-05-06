290828a384e9775588fa49f6d4f9f33f7d4c519a
koreait-shopping <icepri3535@gmail.com>
2022-05-06 17:15:31 +0900

    Silver5 [기본 수학 1 진행중(+새싹), TODO 셀프 넘버(review), 한수(review), 문자열 반복(review), 단어 공부(review),단어의 개수(review), 그룹 단어 체커(review), 분수찾기(review), ACM 호텔(review), 부녀회장이 될테야(review), 설탕 배달(review) 기본수학2]

--- src/HistoryMath1.java
+++ src/HistoryMath1.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class HistoryMath1 {
@@ -165,5 +166,22 @@ public class HistoryMath1 {
         else if (N % 5 == 2 || N % 5 == 4) {
             System.out.println((N / 5) + 2);
         }
+    }
+
+    //두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.9223372036854775807 9223372036854775808
+
+    public static void bigInteger(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        A = A.add(B);
+
+        System.out.println(A.toString());
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -6,19 +8,13 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        if (N == 4 || N == 7) {
-            System.out.println(-1);
-        }
-        else if (N % 5 == 0) {
-            System.out.println(N / 5);
-        }
-        else if (N % 5 == 1 || N % 5 == 3) {
-            System.out.println((N / 5) + 1);
-        }
-        else if (N % 5 == 2 || N % 5 == 4) {
-            System.out.println((N / 5) + 2);
-        }
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        A = A.add(B);
+
+        System.out.println(A.toString());
     }
 }
