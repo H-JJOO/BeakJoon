b737ca697864245b9b1581786bb9243b3ab0038e
koreait-shopping <icepri3535@gmail.com>
2022-05-08 21:26:52 +0900

    Silver5 [기본 수학 1 진행중(+새싹), TODO 셀프 넘버(review), 한수(review), 문자열 반복(review), 단어 공부(review),단어의 개수(review), 그룹 단어 체커(review), 분수찾기(review), ACM 호텔(review), 부녀회장이 될테야(review), 설탕 배달(review) , 기본 수학2 소수 찾기(review), 소수(review)]

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,47 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
+
+    public static boolean prime[];
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        br.readLine();
-        int cnt = 0;
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        prime = new boolean[b + 1];//배열 생성
+        get_prime();
 
-        while (st.hasMoreTokens()) {
-            boolean isPrime = true;
-
-            int num = Integer.parseInt(st.nextToken());
-
-            if (num == 1) {
-                continue;
-            }
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    isPrime = false;
-                    break;
+        //소수 합 및 최솟값
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = a; i <= b; i++) {
+            if (prime[i] == false) {//소수
+                sum += i;
+                if (min == Integer.MAX_VALUE) {//첫 소수가 최솟값
+                    min = i;
                 }
             }
-            if (isPrime) {
-                cnt++;
+        }
+        if (sum == 0) {//소수가 없다면
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+    public static void get_prime() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if (prime[i]) continue;//이미 체크 된 배열일 경우 생략
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
             }
         }
-        System.out.println(cnt);
     }
 }
