3de0a7d7f741c985345dcf6bfe3638f4eda9ffae
koreait-shopping <icepri3535@gmail.com>
2022-05-09 23:04:10 +0900

    Silver5 [기본 수학 1 진행중(+새싹), TODO 셀프 넘버(review), 한수(review), 문자열 반복(review), 단어 공부(review),단어의 개수(review), 그룹 단어 체커(review), 분수찾기(review), ACM 호텔(review), 부녀회장이 될테야(review), 설탕 배달(review) , 기본 수학2 소수 찾기(review), 소수(review), 소인수분해(review), 소수 구하기]

--- src/HistoryMath1.java
+++ src/HistoryMath1.java
@@ -215,4 +215,72 @@ public class HistoryMath1 {
         }
         System.out.println(cnt);
     }
+
+
+    //자연수 M과 N이 주어질 때 M이상 N이하의 자연수 중 소수인 것을 모두 골라 이들 소수의 합과 최솟값을 찾는 프로그램을 작성하시오.
+    //예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 소수는 61, 67, 71, 73, 79, 83, 89, 97 총 8개가 있으므로, 이들 소수의 합은 620이고, 최솟값은 61이 된다.
+
+    public static boolean prime[];
+
+    public static void decimal2(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        prime = new boolean[b + 1];//배열 생성
+        get_prime();
+
+        //소수 합 및 최솟값
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = a; i <= b; i++) {
+            if (prime[i] == false) {//소수
+                sum += i;
+                if (min == Integer.MAX_VALUE) {//첫 소수가 최솟값
+                    min = i;
+                }
+            }
+        }
+        if (sum == 0) {//소수가 없다면
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+    public static void get_prime() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if (prime[i]) continue;//이미 체크 된 배열일 경우 생략
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+
+    //정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+    public static void primeFactorization(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            while (a % i == 0) {
+                sb.append(i).append('\n');
+                a /= i;
+            }
+        }
+        if (a != 1) {
+            sb.append(a);
+        }
+        System.out.println(sb);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,23 @@ import java.io.*;
 
 public class Main {
 
-    public static boolean prime[];
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringBuilder sb = new StringBuilder();
+
         int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
 
-        prime = new boolean[b + 1];//배열 생성
-        get_prime();
-
-        //소수 합 및 최솟값
-        int sum = 0;
-        int min = Integer.MAX_VALUE;
-        for (int i = a; i <= b; i++) {
-            if (prime[i] == false) {//소수
-                sum += i;
-                if (min == Integer.MAX_VALUE) {//첫 소수가 최솟값
-                    min = i;
-                }
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            while (a % i == 0) {
+                sb.append(i).append('\n');
+                a /= i;
             }
         }
-        if (sum == 0) {//소수가 없다면
-            System.out.println(-1);
-        } else {
-            System.out.println(sum);
-            System.out.println(min);
+        if (a != 1) {
+            sb.append(a);
         }
-    }
-
-    public static void get_prime() {
-        prime[0] = true;
-        prime[1] = true;
-
-        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
-            if (prime[i]) continue;//이미 체크 된 배열일 경우 생략
-            for (int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
-            }
-        }
+        System.out.println(sb);
     }
 }
